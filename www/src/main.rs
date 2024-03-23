212ecc94a62ca74cf71c6cfa19d0cd73d371a9fd
Matthew Rogers <alula_irl@proton.me>
2024-03-22 19:47:13 -0500

    handle base tcp connections

--- www/src/main.rs
+++ www/src/main.rs
@@ -16,7 +16,6 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
-
         handle_connection(stream);
     }
 }
@@ -25,34 +24,39 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
-        .map(|result| result.unwrap_or_else(|err| {
-            eprintln!("Error reading line: {}", err);
-            String::new() // Return an empty string in case of error
-        }))
+        .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
     println!("{:#?}", http_request);
-    let request_line = &http_request.first();
-    let request_line_parts: Vec<_> = request_line.unwrap().split(" ").collect();
-    let directory = request_line_parts[1];
-    println!("{}", directory);
-    let contents;
+    if http_request.len() > 0 {
+        let request_line = &http_request.first();
+        let request_line_parts: Vec<_> = request_line.unwrap().split(" ").collect();
+        let directory = request_line_parts[1];
+        println!("{}", directory);
+        let contents;
 
-    if directory == "/favicon.ico" {
-        contents = fs::read("static/icons/favicon.ico").unwrap();
+        if directory == "/favicon.ico" {
+            contents = fs::read("static/icons/favicon.ico").unwrap();
 
+        } else {
+            contents = fs::read("content/index.html").unwrap();
+        }
+
+        let length = contents.len();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n",
+            length
+        );
+
+        let _ = stream.write_all(response.as_bytes());
+        let _ = stream.write_all(&contents);
     } else {
-        contents = fs::read("content/index.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 400 Bad Request\r\nContent-Length: {}\r\n\r\n", 0
+        );
+        let _ = stream.write_all(response.as_bytes());
     }
-
-    let length = contents.len();
-
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n",
-        length
-    );
-
-    let _ = stream.write_all(response.as_bytes());
-    let _ = stream.write_all(&contents);
+    
 }
